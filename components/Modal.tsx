6a2c6433418d7e15fa66b32afc4520fafe4ca2c4
Christerresin <christer.resin@gmail.com>
2022-05-16 12:59:42 +0200

    add state for trailer and genres

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,15 +3,19 @@ import MuiModal from '@mui/material/Modal'
 import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 
-import { modalState } from '../atoms/modalAtom'
-import { Movie } from '../typings'
+import { modalState, movieState } from '../atoms/modalAtom'
+import { Element, Genre } from '../typings'
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState)
-  const [movie, setMovie] = useState<Movie | null>(null)
+  const [movie, setMovie] = useRecoilState(movieState)
+  const [trailer, setTrailer] = useState('')
+  const [genres, setGenres] = useState<Genre[]>([])
 
   useEffect(() => {
-    if (!movie) return
+    if (!movie) {
+      return
+    }
 
     async function fetchMovie() {
       const data = await fetch(
@@ -20,13 +24,26 @@ function Modal() {
         }/${movie?.id}?api_key=${
           process.env.NEXT_PUBLIC_API_KEY
         }&language=en-US&append_to_response=videos`
-      )
+      ).then((response) => response.json())
+
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === 'Trailer'
+        )
+        setTrailer(data.videos?.results[index]?.key)
+      }
+      if (data?.genres) {
+        setGenres(data.genres)
+      }
     }
-  }, [])
+    fetchMovie()
+  }, [movie])
 
   const handleClose = () => {
     setShowModal(false)
   }
+
+  console.log(trailer)
 
   return (
     <MuiModal open={showModal} onClose={handleClose}>
@@ -35,7 +52,7 @@ function Modal() {
           onClick={handleClose}
           className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
         >
-          <XIcon className="h6 w-6" />
+          <XIcon className="h-6 w-6" />
         </button>
 
         <div></div>
