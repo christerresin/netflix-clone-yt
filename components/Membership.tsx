de1b0746eb5e9824eb80e35aa80955e4ca6d825f
Christerresin <christer.resin@gmail.com>
2022-05-20 12:40:11 +0200

    add cancel sub logic in Membership comp

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,16 +1,53 @@
+import { collection, doc, getDocs, updateDoc } from 'firebase/firestore'
 import { useState } from 'react'
 import { useRecoilValue } from 'recoil'
-import { planState } from '../atoms/subscriptionAtom'
-import useAuth from '../hooks/useAuth'
+import { useRouter } from 'next/router'
+
 import Loader from './Loader'
+import useAuth from '../hooks/useAuth'
+import { db } from '../firebase'
+
+import { planState, subscriptionState } from '../atoms/subscriptionAtom'
 
 function Membership() {
   const { user } = useAuth()
   const [isBillingLoading, setIsBillingLoading] = useState(false)
   const userPlan = useRecoilValue(planState)
-  const subscription = false
+  const userSubscription = useRecoilValue(subscriptionState)
+  const router = useRouter()
 
-  const manageSubscription = () => {
+  const manageSubscription = async () => {
+    const updateFoundUser = async (foundUserObj: {
+      docId: string
+      email: string
+      uid: string
+      subscription: string
+      subscriptionDate: number
+    }) => {
+      const userRef = doc(db, 'users', foundUserObj.docId)
+      await updateDoc(userRef, {
+        subscription: '',
+      })
+      setIsBillingLoading(false)
+    }
+
+    const querySnapshot = await getDocs(collection(db, 'users'))
+    querySnapshot.forEach((document) => {
+      if (user?.uid === document.data().uid) {
+        let foundUser = {
+          uid: document.data().uid,
+          email: document.data().email,
+          subscription: document.data().subscription,
+          docId: document.id,
+          subscriptionDate: document.data().subscriptionDate,
+        }
+
+        if (foundUser) {
+          updateFoundUser(foundUser)
+          router.push('/')
+        }
+      }
+    })
     setIsBillingLoading(true)
   }
 
@@ -19,7 +56,7 @@ function Membership() {
       <div className="space-y-2 py-4">
         <h4 className="text-lg text-[gray]">Membership & Billing</h4>
         <button
-          disabled={isBillingLoading || !subscription}
+          disabled={isBillingLoading || !userSubscription}
           className="h-10 w-3/5 whitespace-nowrap bg-gray-300 py-2 text-sm font-medium text-black shadow-md hover:bg-gray-200 md:w-4/5"
           onClick={manageSubscription}
         >
@@ -43,7 +80,7 @@ function Membership() {
           </div>
         </div>
 
-        <div>
+        <div className="flex flex-col justify-between pt-4 pb-4 md:flex-row md:pb-0">
           <div>
             <p>
               {userPlan
@@ -52,6 +89,12 @@ function Membership() {
                   ).toDateString()}`
                 : null}
             </p>
+          </div>
+          <div className="md:text-right">
+            <p className="membershipLink">Manage payment info</p>
+            <p className="membershipLink">Add backup payment method</p>
+            <p className="membershipLink">Billing Details</p>
+            <p className="membershipLink">Change billing day</p>
           </div>
         </div>
       </div>
